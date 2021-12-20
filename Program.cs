092084d77b4ef08918300957ac577bfe3fae14ec
Fernando Maciel <macielprojects@outlook.com>
2021-12-20 10:51:15 -0300

    Converting

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace HTTP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var requisicao = WebRequest.Create("https://jsonplaceholder.typicode.com/todos");
+            requisicao.Method = "GET";
+            
+            using (var resposta = requisicao.GetResponse()) 
+            {
+               var stream = resposta.GetResponseStream();
+               StreamReader leitor = new StreamReader(stream);
+               object dados = leitor.ReadToEnd();
+
+               List<Tarefa> Tarefas = JsonConvert.DeserializeObject<List<Tarefa>>(dados.ToString());
+               foreach(Tarefa tarefa in Tarefas)
+                {
+                    tarefa.Exibir();
+                }
+
+               stream.Close();
+               resposta.Close();
+            }
+               Console.ReadLine();
+        }
+
+    }
+}
---
+++ Tarefa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTP
+{
+    internal class Tarefa
+    {
+        public int UserId;
+        public int id;
+        public string title;
+        public bool completed;
+
+
+        public void Exibir()
+        {
+            Console.WriteLine("Objeto Tarefa");
+            Console.WriteLine($"User Id:{UserId}");
+            Console.WriteLine($"id:{id}");
+            Console.WriteLine($"title:{title}");
+            Console.WriteLine($"Finalizou?:{completed}");
+            Console.WriteLine("");
+            Console.WriteLine("====================================");
+        }
+    }
+
+
+
+}
